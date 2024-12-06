1fefc0d05f08551a2c5127796fa5016dabd89db9
sukmo <sukmo.dev@gmail.com>
2024-12-07 05:32:00 +0700

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Sukmo Ronggo Hadi Wijoyo on 07/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
